b43f594de2de17de13ea90aa0a8b66499ff17605
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2019-10-07 15:19:34 +0300

    Office adding completed

--- core/pclass/other/Mysql.php
+++ core/pclass/other/Mysql.php
@@ -6,9 +6,7 @@ class Mysql extends mysqli {
     static public $db_pass;
     static public $db_name;
 
-    const cns_create = 'CREATE TABLE IF NOT EXISTS';
-    const cns_prefix = 'Sdy';
-    const cns_charset = 'utf8_general_ci';
+    const PREFIX = 'Sdy';
 
     public function __construct() {
         parent::__construct(static::$db_host, static::$db_user, static::$db_pass, static::$db_name);
@@ -24,13 +22,42 @@ class Mysql extends mysqli {
             $this->close();
         }
     }
-
-    public function Begin() {
-        return 'Mysql Ret';
+    public function ErrorHandling($Mysql) {
+        if (!empty($Mysql->error)) {
+            echo '<pre>';
+            print_r($Mysql->error);
+            echo '</pre>';
+            exit;
+        }
+        return true;
     }
 
-    private function CreateDB() {
-        $query = 'CREATE SCHEMA `Sdy` DEFAULT CHARACTER SET utf8 ';
-    }
+    public function CreateOfficeTable() {
+        $query = 'CREATE TABLE IF NOT EXISTS `speedy_office` (
+            `ID` int(10) UNSIGNED NOT NULL,
+            `OfficeID` int(10) UNSIGNED NOT NULL,
+            `Title` varchar(200) NOT NULL,
+            `Address` varchar(200) DEFAULT NULL,
+            `CountryID` int(11) DEFAULT NULL,
+            `SiteID` int(11) DEFAULT NULL,
+            `PostCode` int(11) DEFAULT NULL,
+            `StreetId` int(11) DEFAULT NULL,
+            `QuarterId` int(11) DEFAULT NULL,
+            `CoordTypeId` int(11) DEFAULT NULL,
+            `CoordX` double DEFAULT NULL,
+            `CoordY` double DEFAULT NULL,
+            `Closed` tinyint(4) NOT NULL
+        ) ENGINE = InnoDB DEFAULT CHARSET = utf8;';
+        $this->query($query);
 
+        $query = 'ALTER TABLE `speedy_office` ADD PRIMARY KEY (`ID`); ';
+        $this->query($query);
+
+        $query = ' ALTER TABLE `speedy_office` MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;';
+        $this->query($query);
+
+        $this->ErrorHandling($this);
+
+        return true;
+    }
 }
---
+++ core/pclass/other/SExtractData.php
@@ -0,0 +1,76 @@
+<?php
+
+class Soffice {
+    const BDOFFICE = 'speedy_office';
+    const DBCITY = 'speedy_city';
+
+    public function Working() {
+        $Mysql = new MySQL();
+        $Mysql->CreateOfficeTable();
+        // $Mysql->CreateDB($this->DBCITY);
+
+        $username = Sdy::$login;
+        $password = Sdy::$password;
+        $server = Sdy::$server;
+        $eps = new EPSFacade(new EPSSOAPInterfaceImpl($server), $username, $password);
+        $name = null;
+
+        // list countries
+        // $countryList = $eps->listCountries($name);
+        // Austria, no results for any country
+        // $arrResultOfficesEx = $eps->listOfficesEx($name, null, null, 40);
+
+        $arrResultOfficesEx = $eps->listOfficesEx($name);
+        $arrJsonResult = array();
+        $count = count($arrResultOfficesEx);
+        if (!empty($count)) {
+            // default all offices to be closed - not working anymore, but keep in the DB
+            $Mysql->query('UPDATE ' . self::BDOFFICE . ' SET Closed = 1');
+            for ($i = 0; $i < count($arrResultOfficesEx); $i++) {
+                $resultOfficeEx = $arrResultOfficesEx[$i];
+                $query = 'SELECT * FROM ' . self::BDOFFICE . ' WHERE OfficeID = ' . $resultOfficeEx->getId();
+                $officeDB = $Mysql->query($query);
+                $this->AddOrUpdateOffice($Mysql, $officeDB, $resultOfficeEx);
+            }
+            unset($Mysql);
+        }
+        return true;
+    }
+
+    private function AddOrUpdateOffice($Mysql, $officeDB, $record) {
+        $countryId = 100; // Bulgaria
+        $of_id = $record->getId();
+
+        if ($officeDB->num_rows > 0) {
+            $query = 'UPDATE ' . self::BDOFFICE . ' SET Closed = 0 WHERE OfficeID = ' . $of_id;
+        } else {
+            $query = 'INSERT INTO ' . self::BDOFFICE;
+            $query .= ' (OfficeID, Title, Address, CountryID, SiteID, PostCode, StreetId, QuarterId, CoordTypeId, CoordX, CoordY, Closed) VALUES ';
+            $resultOfficeAddressEx = $record->getAddress();
+            $resultOfficeSite = $resultOfficeAddressEx->getResultSite();
+            $of_title = $record->getName();
+            $of_siteid = $resultOfficeAddressEx->getResultSite()->getId();
+            $of_full_address = $resultOfficeAddressEx->getFullAddressString();
+            $of_full_address = $Mysql->escape_string($of_full_address);
+            $of_postcode = $resultOfficeAddressEx->getPostCode();
+            $of_StreetId = $resultOfficeAddressEx->getStreetId();
+            $of_StreetId = (empty($of_StreetId)) ? 0 : $of_StreetId;
+            $of_QuarterId = $resultOfficeAddressEx->getQuarterId();
+            $of_QuarterId = (empty($of_QuarterId)) ? 0 : $of_QuarterId;
+            $of_CoordX = $resultOfficeAddressEx->getCoordX();
+            $of_CoordY = $resultOfficeAddressEx->getCoordY();
+            $of_CoordTypeId = $resultOfficeAddressEx->getCoordTypeId();
+            $of_closed = 0;
+
+            $query .= '("' . $of_id . '", "' . $of_title . '", "' . $of_full_address . '", ';
+            $query .= '"' . $countryId . '", "' . $of_siteid . '", ';
+            $query .= '"' . $of_postcode . '", "' . $of_StreetId . '", ';
+            $query .= '"' . $of_QuarterId . '", "' . $of_CoordTypeId . '", ';
+            $query .= '"' . $of_CoordX . '", "' . $of_CoordY . '", "' . $of_closed . '")';
+        }
+        $Mysql->query($query);
+        $Mysql->ErrorHandling($Mysql);
+
+        return true;
+    }
+}
