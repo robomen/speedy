b123a053bdfcf6c487fb9089b9ef8afa1e6fcb86
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2019-10-08 12:19:37 +0300

    Added Citis, Quarters and Streets

--- core/pclass/other/Mysql.php
+++ core/pclass/other/Mysql.php
@@ -34,28 +34,76 @@ class Mysql extends mysqli {
 
     public function CreateOfficeTable() {
         $query = 'CREATE TABLE IF NOT EXISTS `speedy_office` (
-            `ID` int(10) UNSIGNED NOT NULL,
+            `ID` int(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
             `OfficeID` int(10) UNSIGNED NOT NULL,
             `Title` varchar(200) NOT NULL,
             `Address` varchar(200) DEFAULT NULL,
-            `CountryID` int(11) DEFAULT NULL,
-            `SiteID` int(11) DEFAULT NULL,
-            `PostCode` int(11) DEFAULT NULL,
-            `StreetId` int(11) DEFAULT NULL,
-            `QuarterId` int(11) DEFAULT NULL,
-            `CoordTypeId` int(11) DEFAULT NULL,
+            `CountryID` int(11) UNSIGNED DEFAULT NULL,
+            `SiteID` int(11) UNSIGNED DEFAULT NULL,
+            `PostCode` int(11) UNSIGNED DEFAULT NULL,
+            `StreetId` int(11) UNSIGNED DEFAULT NULL,
+            `QuarterId` int(11) UNSIGNED DEFAULT NULL,
             `CoordX` double DEFAULT NULL,
             `CoordY` double DEFAULT NULL,
-            `Closed` tinyint(4) NOT NULL
+            `CoordTypeID` int(11) UNSIGNED DEFAULT NULL,
+            `Closed` tinyint(1) NOT NULL
         ) ENGINE = InnoDB DEFAULT CHARSET = utf8;';
         $this->query($query);
+        $this->ErrorHandling($this);
 
-        $query = 'ALTER TABLE `speedy_office` ADD PRIMARY KEY (`ID`); ';
+
+        return true;
+    }
+
+    public function CreateStreetTable() {
+        $query = 'CREATE TABLE IF NOT EXISTS `speedy_street` (
+            `ID` int(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+            `StreetID` int(10) UNSIGNED NOT NULL,
+            `Type` varchar(10) NOT NULL,
+            `Name` varchar(255) NOT NULL,
+            `PostCode` int(11) UNSIGNED NOT NULL,
+            `SiteID` int(11) UNSIGNED NOT NULL
+        ) ENGINE = InnoDB DEFAULT CHARSET = utf8;';
         $this->query($query);
+        $this->ErrorHandling($this);
 
-        $query = ' ALTER TABLE `speedy_office` MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;';
+        return true;
+    }
+
+    public function CreateQuarterTable() {
+        $query = 'CREATE TABLE IF NOT EXISTS `speedy_quarter` (
+            `ID` int(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+            `QuarterID` int(10) UNSIGNED NOT NULL,
+            `Type` varchar(10) NOT NULL,
+            `Name` varchar(255) NOT NULL,
+            `PostCode` int(11) UNSIGNED NOT NULL,
+            `SiteID` int(11) UNSIGNED NOT NULL
+        ) ENGINE = InnoDB DEFAULT CHARSET = utf8;';
         $this->query($query);
+        $this->ErrorHandling($this);
 
+        return true;
+    }
+
+    public function CreateCityTable() {
+        $query = 'CREATE TABLE IF NOT EXISTS `speedy_city` (
+            `ID` int(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+            `CityID` int(10) UNSIGNED NOT NULL,
+            `Type` varchar(10) NOT NULL,
+            `Name` varchar(120) DEFAULT NULL,
+            `Municpality` varchar(120) DEFAULT NULL,
+            `Region` varchar(120) DEFAULT NULL,
+            `PostCode` int(11) UNSIGNED DEFAULT NULL,
+            `AddrN` varchar(120) DEFAULT NULL,
+            `CountryID` int(11) UNSIGNED DEFAULT NULL,
+            `ServingOfficeID` int(3) UNSIGNED DEFAULT NULL,
+            `CoordX` double DEFAULT NULL,
+            `CoordY` double DEFAULT NULL,
+            `CoordTypeID` int(11) UNSIGNED DEFAULT NULL,
+            `ServingDays` varchar(7) NOT NULL,
+            `Closed` tinyint(1) NOT NULL
+        ) ENGINE = InnoDB DEFAULT CHARSET = utf8;';
+        $this->query($query);
         $this->ErrorHandling($this);
 
         return true;
--- core/pclass/other/SExtractData.php
+++ core/pclass/other/SExtractData.php
@@ -2,12 +2,20 @@
 
 class SExtractData {
     const BDOFFICE = 'speedy_office';
+    const DBSTREET = 'speedy_street';
+    const DBQUARTER = 'speedy_quarter';
     const DBCITY = 'speedy_city';
 
     public function Working() {
         $Mysql = new MySQL();
         $Mysql->CreateOfficeTable();
-        // $Mysql->CreateDB($this->DBCITY);
+        $Mysql->CreateStreetTable();
+        $Mysql->CreateQuarterTable();
+        $Mysql->CreateCityTable();
+
+        // clear street table
+        $Mysql->query('TRUNCATE ' . self::DBSTREET);
+        $Mysql->query('TRUNCATE ' . self::DBQUARTER);
 
         $username = Sdy::$login;
         $password = Sdy::$password;
@@ -16,61 +24,151 @@ class SExtractData {
         $name = null;
 
         // list countries
-        $countryList = $eps->listCountriesEx($name);
-        echo '<pre>';
-        print_r($countryList);
-        echo '</pre>';
-        exit;
+        // $countryList = $eps->listCountriesEx($name);
+        // echo '<pre>';
+        // print_r($countryList);
+        // echo '</pre>';
+        // exit;
         // Austria, no results for any country
-        $arrResultOfficesEx = $eps->listOfficesEx($name, null, null, 40);
+        // $arrResultOfficesEx = $eps->listOfficesEx($name, null, null, 40);
 
-        $arrResultOfficesEx = $eps->listOfficesEx($name);
-        $arrJsonResult = array();
-        $count = count($arrResultOfficesEx);
-        if (!empty($count)) {
+        // add cities
+        $cities = $eps->listAllSites('BG', 100);
+        if (!empty($cities)) {
+            foreach ($cities as $city) {
+                $query = 'SELECT * FROM ' . self::DBCITY . ' WHERE CityID = ' . $city->getId();
+                $cityDB = $Mysql->query($query);
+                $this->AddOrUpdateCity($cityDB, $city);
+                $citySiteID = $city->getId();
+                $cityPostCode = $city->getPostCode();
+                // add streets
+                $this->AddStreetOrQuarter($eps, $citySiteID, $cityPostCode, 'street');
+                // add quarters
+                $this->AddStreetOrQuarter($eps, $citySiteID, $cityPostCode, 'quarter');
+            }
+        }
+
+        // add offices
+        $resultOffices = $eps->listOfficesEx($name);
+        if (!empty($resultOffices)) {
             // default all offices to be closed - not working anymore, but keep in the DB
             $Mysql->query('UPDATE ' . self::BDOFFICE . ' SET Closed = 1');
-            for ($i = 0; $i < count($arrResultOfficesEx); $i++) {
-                $resultOfficeEx = $arrResultOfficesEx[$i];
-                $query = 'SELECT * FROM ' . self::BDOFFICE . ' WHERE OfficeID = ' . $resultOfficeEx->getId();
+            foreach ($resultOffices as $office) {
+                $query = 'SELECT * FROM ' . self::BDOFFICE . ' WHERE OfficeID = ' . $office->getId();
                 $officeDB = $Mysql->query($query);
-                $this->AddOrUpdateOffice($Mysql, $officeDB, $resultOfficeEx);
+                $this->AddOrUpdateOffice($officeDB, $office);
             }
             unset($Mysql);
         }
         return true;
     }
 
-    private function AddOrUpdateOffice($Mysql, $officeDB, $record) {
+    private function AddOrUpdateOffice($officeDB, $record) {
+        $Mysql = new MySQL();
         $countryId = 100; // Bulgaria
-        $of_id = $record->getId();
+        $ofID = $record->getId();
 
         if ($officeDB->num_rows > 0) {
-            $query = 'UPDATE ' . self::BDOFFICE . ' SET Closed = 0 WHERE OfficeID = ' . $of_id;
+            $query = 'UPDATE ' . self::BDOFFICE . ' SET Closed = 0 WHERE OfficeID = ' . $ofID;
         } else {
             $query = 'INSERT INTO ' . self::BDOFFICE;
-            $query .= ' (OfficeID, Title, Address, CountryID, SiteID, PostCode, StreetId, QuarterId, CoordTypeId, CoordX, CoordY, Closed) VALUES ';
+            $query .= ' (OfficeID, Title, Address, CountryID, SiteID, PostCode, StreetId, QuarterId, CoordX, CoordY, CoordTypeID, Closed) VALUES ';
             $resultOfficeAddressEx = $record->getAddress();
-            $resultOfficeSite = $resultOfficeAddressEx->getResultSite();
-            $of_title = $record->getName();
-            $of_siteid = $resultOfficeAddressEx->getResultSite()->getId();
-            $of_full_address = $resultOfficeAddressEx->getFullAddressString();
-            $of_full_address = $Mysql->escape_string($of_full_address);
-            $of_postcode = $resultOfficeAddressEx->getPostCode();
-            $of_StreetId = $resultOfficeAddressEx->getStreetId();
-            $of_StreetId = (empty($of_StreetId)) ? 0 : $of_StreetId;
-            $of_QuarterId = $resultOfficeAddressEx->getQuarterId();
-            $of_QuarterId = (empty($of_QuarterId)) ? 0 : $of_QuarterId;
-            $of_CoordX = $resultOfficeAddressEx->getCoordX();
-            $of_CoordY = $resultOfficeAddressEx->getCoordY();
-            $of_CoordTypeId = $resultOfficeAddressEx->getCoordTypeId();
-            $of_closed = 0;
+            $ofTitle = $record->getName();
+            $ofSiteid = $resultOfficeAddressEx->getResultSite()->getId();
+            $ofFullAddress = $resultOfficeAddressEx->getFullAddressString();
+            $ofFullAddress = $Mysql->escape_string($ofFullAddress);
+            $ofPostcode = $resultOfficeAddressEx->getPostCode();
+            $ofStreetId = $resultOfficeAddressEx->getStreetId();
+            $ofStreetId = (empty($ofStreetId)) ? 0 : $ofStreetId;
+            $ofQuarterId = $resultOfficeAddressEx->getQuarterId();
+            $ofQuarterId = (empty($ofQuarterId)) ? 0 : $ofQuarterId;
+            $ofCoordX = $resultOfficeAddressEx->getCoordX();
+            $ofCoordY = $resultOfficeAddressEx->getCoordY();
+            $ofCoordTypeId = $resultOfficeAddressEx->getCoordTypeId();
+            $ofClosed = 0;
 
-            $query .= '("' . $of_id . '", "' . $of_title . '", "' . $of_full_address . '", ';
-            $query .= '"' . $countryId . '", "' . $of_siteid . '", ';
-            $query .= '"' . $of_postcode . '", "' . $of_StreetId . '", ';
-            $query .= '"' . $of_QuarterId . '", "' . $of_CoordTypeId . '", ';
-            $query .= '"' . $of_CoordX . '", "' . $of_CoordY . '", "' . $of_closed . '")';
+            $query .= '("' . $ofID . '", "' . $ofTitle . '", "' . $ofFullAddress . '", "' . $countryId . '", "' . $ofSiteid . '", "' . $ofPostcode . '", "' . $ofStreetId . '", "' . $ofQuarterId . '", "' . $ofCoordTypeId . '", "' . $ofCoordX . '", "' . $ofCoordY . '", "' . $ofClosed . '")';
+        }
+        $Mysql->query($query);
+        $Mysql->ErrorHandling($Mysql);
+
+        return true;
+    }
+
+    private function AddStreetOrQuarter($eps, $siteId, $postCode, $table) {
+        $Mysql = new MySQL();
+        $name = null;
+
+        if ($table == 'street') {
+            $tableDB = self::DBSTREET;
+            $columns = '(StreetID, Type, Name, PostCode, SiteID)';
+            $records = $eps->listStreets($name, $siteId);
+        } elseif ($table == 'quarter') {
+            $tableDB = self::DBQUARTER;
+            $columns = '(QuarterID, Type, Name, PostCode, SiteID)';
+            $records = $eps->listQuarters($name, $siteId);
+        } else {
+            echo '<pre>';
+            print_r('error table name');
+            echo '</pre>';
+            exit;
+        }
+
+        foreach ($records as $record) {
+            $id = $record->getId();
+            $type = $record->getType();
+            $name = $record->getName();
+            // $stActualName = $record->getActualName();
+            if ($table == 'street') {
+                $select = 'SELECT * FROM ' . self::DBSTREET . ' WHERE StreetID = ' . $id;
+            } elseif ($table == 'quarter') {
+                $select = 'SELECT * FROM ' . self::DBQUARTER . ' WHERE QuarterID = ' . $id;
+            }
+            $row = $Mysql->query($select);
+
+            if ($row->num_rows == 0) {
+                $query = 'INSERT INTO ' . $tableDB . ' ' . $columns . ' VALUES ';
+                $query .= '("' . $id . '", "' . $type . '", "' . $name . '", "' . $postCode . '", "' . $siteId . '")';
+
+                $Mysql->query($query);
+                $Mysql->ErrorHandling($Mysql);
+            }
+
+            $query = null;
+        }
+
+        return true;
+    }
+
+    private function AddOrUpdateCity($cityDB, $record) {
+        $Mysql = new MySQL();
+        $id = $record->getId();
+
+        if ($cityDB->num_rows > 0) {
+            $query = 'UPDATE ' . self::DBCITY . ' SET Closed = 0 WHERE CityID = ' . $id;
+        } else {
+            $query = 'INSERT INTO ' . self::DBCITY;
+            $query .= ' (CityID, Type, Name, Municpality, Region, PostCode, AddrN, CountryID, ServingOfficeID, CoordX, CoordY, CoordTypeID, ServingDays, Closed) VALUES ';
+            $type = $record->getType();
+            $name = $record->getName();
+            $municipality = $record->getMunicipality();
+            $region = $record->getRegion();
+            $postCode = $record->getPostCode();
+            $addrNomen = $record->getAddrNomen()->getValue();
+            $countryId = $record->getCountryId();
+            $servingOfficeId = $record->getServingOfficeId();
+            $coordX = $record->getCoordX();
+            $coordy = $record->getCoordY();
+            $coordtype = $record->getCoordType();
+            $servingdays = $record->getServingDays();
+            $closed = 0;
+
+            if (empty($postCode)) {
+                $postCode = 0;
+            }
+
+            $query .= '("' . $id . '", "' . $type . '", "' . $name . '", "' . $municipality . '", "' . $region . '", "' . $postCode . '", "' . $addrNomen . '", "' . $countryId . '", "' . $servingOfficeId . '", "' . $coordX . '", "' . $coordy . '", "' . $coordtype . '", "' . $servingdays . '", "' . $closed . '")';
         }
         $Mysql->query($query);
         $Mysql->ErrorHandling($Mysql);
